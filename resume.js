f35ff9cf51b3d5e8b43ef86a449195b11913515d
Edward Silva <easilva@mines.edu>
2024-12-29 20:06:49 -0500

    updated sitemap

--- resume.js
+++
@@ -1,8 +0,0 @@
-function downloadResume() {
-    const link = document.createElement('a');
-    link.href = 'resume.pdf';
-    link.download = 'EDWARD_SILVA_RESUME_2024.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
